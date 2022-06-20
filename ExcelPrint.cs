11bda96562bd84d14459dbc2574170b794fa8bf0
Joshua Goldman <jogo@Gavle.Syntronic.se>
2022-06-21 01:33:00 +0200

    Class representing the excel info to be printed

---
+++ ExcelPrint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintMailExcel
+{
+    public class ExcelPrint
+    {
+        public ExcelPrint(string filename, string sheetName, string content)
+        {
+            Filename = filename;
+            SheetName = sheetName;
+            Content = content;
+        }
+
+        public string Filename { get; }
+        public string SheetName { get; }
+        public string Content { get; }
+    }
+}
