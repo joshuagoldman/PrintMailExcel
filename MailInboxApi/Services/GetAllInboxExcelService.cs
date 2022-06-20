3121dc66d81a040663678337c67dd9438ee0f14b
Joshua Goldman <jogo@Gavle.Syntronic.se>
2022-06-21 01:40:37 +0200

    Service for fetching each excel file with printed excel rows

---
+++ MailInboxApi/Services/GetAllInboxExcelService.cs
@@ -0,0 +1,44 @@
+﻿using MailInboxApi.Data.Repositories;
+using MailInboxApi.DataAccess;
+using PrintMailExcel;
+using MailClient = MailInboxApi.DataAccess.MailClient;
+
+namespace MailInboxApi.Services
+{
+    public class GetAllInboxExcelService : IGetAllInboxExcelService
+    {
+        private readonly IInboxExcelRepository _inboxExcelRepository;
+        private readonly IConfiguration _configuration;
+        public GetAllInboxExcelService(IInboxExcelRepository inboxExcelRepository,
+                                       IConfiguration configuration)
+        {
+            _inboxExcelRepository = inboxExcelRepository;
+            _configuration = configuration;
+        }
+
+        public List<ExcelPrintInfo> GetAllExcelInfos(MailClient client)
+        {
+            PrintMailExcel.MailClient mailHandling = new PrintMailExcel.MailClient(client.EmailAddress,
+                                                                                   client.Password);
+
+            List<ExcelFile> excelInfos = new List<ExcelFile>();
+            if (mailHandling.TryGetExcelFilesInfo(_configuration["Storage:ExcelFiles"], out excelInfos))
+            {
+                List<ExcelPrint> excelPrintInfos = mailHandling.GetRows(excelInfos);
+                IEnumerable<ExcelPrintInfo> excelInfosDb = excelPrintInfos.Select(info => new ExcelPrintInfo
+                {
+                    SheetName = info.SheetName,
+                    Filename = info.Filename,
+                    Info = info.Content
+                });
+
+                _inboxExcelRepository.SaveExcelRows(excelInfosDb.ToList());
+                PrintMailExcel.MailClient.PrintExcelFilesinfoRows(excelInfos);
+
+                return excelInfosDb.ToList();
+            }
+            else
+                throw new Exception("Could not fetch any excel file info from inbox!");
+        }
+    }
+}
---
+++ MailInboxApi/Services/IGetAllInboxExcelService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using MailInboxApi.DataAccess;
+
+namespace MailInboxApi.Services
+{
+    public interface IGetAllInboxExcelService
+    {
+        List<ExcelPrintInfo> GetAllExcelInfos(MailClient client);
+    }
+}
