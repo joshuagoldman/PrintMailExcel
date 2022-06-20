cd884130d5e7080f3374265ca56e33699c13aa05
Joshua Goldman <jogo@Gavle.Syntronic.se>
2022-06-21 01:37:36 +0200

    repository dealing with database interaction of email inbox related database data

---
+++ MailInboxApi/Data/Repositories/IInboxExcelRepository.cs
@@ -0,0 +1,9 @@
+﻿using MailInboxApi.DataAccess;
+
+namespace MailInboxApi.Data.Repositories
+{
+    public interface IInboxExcelRepository
+    {
+        void SaveExcelRows(List<ExcelPrintInfo> client);
+    }
+}
---
+++ MailInboxApi/Data/Repositories/InboxExcelRepository.cs
@@ -0,0 +1,13 @@
+﻿using MailInboxApi.DataAccess;
+using PrintMailExcel;
+
+namespace MailInboxApi.Data.Repositories
+{
+    public class InboxExcelRepository : IInboxExcelRepository
+    {
+        public void SaveExcelRows(List<ExcelPrintInfo> client)
+        {
+            // Perform database action here
+        }
+    }
+}
