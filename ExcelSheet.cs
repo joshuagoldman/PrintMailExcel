2c4bf65527d96c2d0549fcbfd939e802ec3f76a3
Joshua Goldman <jogo@Gavle.Syntronic.se>
2022-06-21 01:31:39 +0200

    Class representing an excel sheet with its cells seen as one table

---
+++ ExcelSheet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintMailExcel
+{
+    public class ExcelSheet
+    {
+        public ExcelSheet(List<List<string>> table, string sheetName)
+        {
+            Table = table;
+            SheetName = sheetName;
+        }
+        public List<List<string>> Table { get; }
+        public string SheetName { get; }
+    }
+}
