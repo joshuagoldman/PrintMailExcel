65b3624faddee1fb0f6d1578a01b8734c9efac60
Joshua Goldman <jogo@Gavle.Syntronic.se>
2022-06-21 01:34:13 +0200

    Controller for mail inbox api

---
+++ MailInboxApi/Controllers/MailInboxController.cs
@@ -0,0 +1,33 @@
+﻿using MailInboxApi.DataAccess;
+using MailInboxApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MailInboxApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MailInboxController : Controller
+    {
+        IGetAllInboxExcelService _getAllInboxExcelService;
+        public MailInboxController(IGetAllInboxExcelService getAllInboxExcelService)
+        {
+            _getAllInboxExcelService = getAllInboxExcelService;
+        }
+
+        [HttpPost("/ExcelFiles")]
+        public IActionResult GetAllInboxExcelFilesInfo(MailClient client)
+        {
+            try
+            {
+                List<ExcelPrintInfo> excelPrintInfo = _getAllInboxExcelService.GetAllExcelInfos(client);
+
+                return Json(excelPrintInfo);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.ToString());
+            }
+        }
+    }
+}
