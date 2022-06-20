eeb90a32d83bd88b5b326a8588a99a525468a3f3
Joshua Goldman <jogo@Gavle.Syntronic.se>
2022-06-21 01:30:14 +0200

    File representing attched excel file from a specific mail

---
+++ ExcelFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintMailExcel
+{
+    public class ExcelFile
+    {
+        public ExcelFile(List<ExcelSheet> sheets, string excelFileName, Guid fileGuid)
+        {
+            Sheets = sheets;
+            ExcelFileName = excelFileName;
+            FileGuid = fileGuid;
+        }
+
+        public List<ExcelSheet> Sheets { get; }
+        public string ExcelFileName { get; }
+        public Guid FileGuid { get; }
+    }
+}
